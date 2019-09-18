4b4fb40ed89b9038f6db412f29be7f721f6a1fc2
bms9294 <bms9294@rit.edu>
2019-09-18 17:33:37 -0400

    Trying to use pytest correctly.
    Turns out life is simpler than I made it out to be.

---
+++ tests/helloWorldApache.py
@@ -0,0 +1,32 @@
+# Created by: bms9294
+#
+# Meant to test the "Hello World" web server for connectivity and proper content.
+
+
+import pytest;
+import requests;
+
+#The testing class for the HelloWorld WebServer
+#Address and port of the WebSite
+ADDRESS = "http://localhost:80";
+
+try:
+    page = requests.get(ADDRESS,timeout=0.5);
+except:
+    page = None;
+
+
+# Make sure the returned status code is 200 for success.
+def test_Code():
+    assert page.status_code == 200;
+
+# Make sure the content of the site is HTML
+def test_Header():
+    #print(page.headers)
+    assert page.headers['content-type'] == 'text/html';
+
+# Make sure the page is displaying the correct content.
+def test_Content():
+    #print(page.text);
+    assert "<h2>Hello World</h2>" in page.text;
+        
