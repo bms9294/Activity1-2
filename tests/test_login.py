2297ee4b83b1e6c131e841548eee5a94465055ff
jxb7393 <47400695+jxb7393@users.noreply.github.com>
2019-10-28 16:49:10 -0400

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,9 +8,13 @@ def test_Correctlogin():
           values = {'username': 'user',
                     'password': 'pass'}
           
-def test_Failedlogin():
-          values = {'username': 'wrong',
-                    'password': 'alsowrong'}
+def test_FailedUserLogin():
+          values = {'username': 'wronguser',
+                    'password': 'pass'}
+          
+def test_FailedPassLogin():
+          values = {'username': 'user',
+                    'password': 'wrongpass'}
           
 r = requests.post(ADDRESS, data=values)
 # check r against something we set later
