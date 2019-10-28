92888cc93f4e8cd6554de33347d20eb4d30dc827
jxb7393 <47400695+jxb7393@users.noreply.github.com>
2019-10-28 17:03:45 -0400

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,18 +3,22 @@ import requests;
 
 ADDRESS = "http://localhost:80";
 
-
+def requesting():         
+          r = requests.post(ADDRESS, data=values)
+          
 def test_Correctlogin():
+          requesting()
           values = {'username': 'user',
                     'password': 'pass'}
           
 def test_FailedUserLogin():
+          requesting()
           values = {'username': 'wronguser',
                     'password': 'pass'}
           
 def test_FailedPassLogin():
+          requesting()
           values = {'username': 'user',
                     'password': 'wrongpass'}
-          
-r = requests.post(ADDRESS, data=values)
+
 # check r against something we set later
