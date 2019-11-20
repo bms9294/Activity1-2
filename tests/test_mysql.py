0cc659c8f2872ed02a9a45eb40e5f46ceac9fb62
bms9294 <bms9294@rit.edu>
2019-11-19 21:51:31 -0500

    NGINX suits out purposes better. Removed semi-colons from python test file.

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,19 +1,19 @@
-import pymysql;
-import pytest;
+import pymysql
+import pytest
 
 def mysql_connect():
     conn=pymysql.connect(host='127.0.0.1',
                          port=33060,
                          user='root',
                          password='cBc9029U',
-                         database='videos4u_web');
-    query = conn.cursor();
-    return query;
+                         database='videos4u_web')
+    query = conn.cursor()
+    return query
 
 
 def test_database():
-    query = mysql_connect();
-    query.execute("SHOW DATABASES");
-    test = query.fetchall();
-    assert ('videos4u_web',) in test;
+    query = mysql_connect()
+    query.execute("SHOW DATABASES")
+    test = query.fetchall()
+    assert ('videos4u_web',) in test
 
