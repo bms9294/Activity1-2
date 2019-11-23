be75050d0fbac9f95575e70a97602ec3ba4085d1
peterc-rit <42870325+peterc-rit@users.noreply.github.com>
2019-11-23 17:08:58 -0500

    hopefully fixing tests

---
+++ tests/test_mysqlClassicInject.py
@@ -0,0 +1,41 @@
+import requests
+import requests.utils
+import pytest
+import pymysql
+import urllib3
+urllib3.disable_warnings()
+
+
+def test_classicSQLInjection():
+    data = {'username': 'jimstark', 'password': 'password1'}
+    try:
+        page = requests.post("https://localhost/core/login/login.php", data=data, verify=False)
+    except:
+        page = None
+    assert page != None
+    assert '{"success": true}' in page.text
+    session = page.headers["Set-Cookie"][8:136]
+
+    file = {'file': open("tests/VideoTest.mp4", 'rb')}
+    inputData = {'filename': 'VideoTest'}
+    try:
+        page = requests.post("https://localhost/core/video/videoUpload.php", files=file, data=inputData, cookies={"session": session}, verify=False)
+    except:
+        page = None    
+    data = page.json()
+    global videoPath
+    videoPath = data['path']
+    
+    assert page != None
+    assert '{"success": true' in page.text
+
+    inputData = {'title': '(SELECT passhash FROM users WHERE username=\"jimstark\")', 'path': videoPath, 'description': 'This is a test video upload'}
+    try:
+        page = requests.post("https://localhost/core/video/finalize.php", data=inputData, verify=False)
+    except:
+        page = None
+    assert page != None
+    assert '{"success": true}' in page.text
+
+
+test_classicSQLInjection()
--- tests/test_videoOperation.py
+++ tests/test_videoOperation.py
@@ -8,8 +8,6 @@ urllib3.disable_warnings()
 session = ""
 videoPath = ""
 
-
-
 def mysql_connect():
     conn=pymysql.connect(host='127.0.0.1',
                          port=33060,
@@ -19,8 +17,15 @@ def mysql_connect():
     query = conn.cursor()
     return query
 
-def test_userLogin():
-    global session
+def getVideoID():
+    query = mysql_connect()
+    query.execute("SELECT videoID FROM videos WHERE pathToVideo=" + "'" + videoPath + "'")
+    vid_id = query.fetchone()
+    return vid_id[0]
+
+
+def test_videoOperation():
+    ### User Login ###
     data = {'username': 'jimstark', 'password': 'password1'}
     try:
         page = requests.post("https://localhost/core/login/login.php",data=data, verify=False)
@@ -30,9 +35,7 @@ def test_userLogin():
     assert '{"success": true}' in page.text
     session = page.headers["Set-Cookie"][8:136]
     
-
-
-def test_uploadVideo():
+    ### Video Upload ###
     file = {'file': open("tests/VideoTest.mp4", 'rb')}
     inputData = {'filename': 'Video Test'}
     try:
@@ -45,8 +48,7 @@ def test_uploadVideo():
     assert page != None
     assert '{"success": true' in page.text
 
-
-def test_finalizeVideo():
+    ### Video Finalize Upload ###
     inputData = {'title': "'Video Test'", 'path': videoPath, 'description': 'This is a test video upload'}
     try:
         page = requests.post("https://localhost/core/video/finalize.php", data=inputData, verify=False)
@@ -55,16 +57,8 @@ def test_finalizeVideo():
     assert page != None
     assert '{"success": true}' in page.text
 
-def getVideoID():
-    query = mysql_connect()
-    query.execute("SELECT videoID FROM videos WHERE pathToVideo=" + "'" + videoPath + "'")
-    vid_id = query.fetchone()
-    return vid_id[0]
-        
-
-def test_deleteVideo():
+    ### Delete Video ###
     idVal = getVideoID()
-    print(idVal)
     inputData = {'video': idVal}
     try:
         page = requests.post("https://localhost/core/deleteVideo.php", data=inputData ,cookies={"session": session}, verify=False)
@@ -74,39 +68,3 @@ def test_deleteVideo():
     assert '{"success": true}' in page.text
 
 
-### CLASSIC INJECTION TEST ###
-
-def test_classicSQLInjection():
-    global session
-    data = {'username': 'jimstark', 'password': 'password1'}
-    try:
-        page = requests.post("https://localhost/core/login/login.php", data=data, verify=False)
-    except:
-        page = None
-    assert page != None
-    assert '{"success": true}' in page.text
-    session = page.headers["Set-Cookie"][8:136]
-
-    file = {'file': open("tests/VideoTest.mp4", 'rb')}
-    inputData = {'filename': 'VideoTest'}
-    try:
-        page = requests.post("https://localhost/core/video/videoUpload.php", files=file, data=inputData, cookies={"session": session}, verify=False)
-    except:
-        page = None    
-    data = page.json()
-    global videoPath
-    videoPath = data['path']
-    
-    assert page != None
-    assert '{"success": true' in page.text
-
-    inputData = {'title': '(SELECT passhash FROM users WHERE username=\"jimstark\")', 'path': videoPath, 'description': 'This is a test video upload'}
-    try:
-        page = requests.post("https://localhost/core/video/finalize.php", data=inputData, verify=False)
-    except:
-        page = None
-    assert page != None
-    assert '{"success": true}' in page.text
-
-
-test_classicSQLInjection()
