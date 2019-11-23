78323781e8c1f9ab4853fddf4fbc962ba0120446
peterc-rit <42870325+peterc-rit@users.noreply.github.com>
2019-11-23 18:41:17 -0500

    hopefully this fixes it

--- tests/test_mysqlClassicInject.py
+++ tests/test_mysqlClassicInject.py
@@ -9,17 +9,17 @@ urllib3.disable_warnings()
 def test_classicSQLInjection():
     data = {'username': 'jimstark', 'password': 'password1'}
     try:
-        page = requests.post("https://localhost/core/login/login.php", data=data, verify=False)
+        page = requests.post(url="https://localhost/core/login/login.php", data=data, verify=False)
     except:
         page = None
     assert page != None
     assert '{"success": true}' in page.text
     session = page.headers["Set-Cookie"][8:136]
 
-    file = {'file': open("tests/VideoTest.mp4", 'rb')}
+    videofile = {'file': open("tests/VideoTest.mp4", 'rb')}
     inputData = {'filename': 'VideoTest'}
     try:
-        page = requests.post("https://localhost/core/video/videoUpload.php", files=file, data=inputData, cookies={"session": session}, verify=False).json()
+        page = requests.post(url="https://localhost/core/video/videoUpload.php", files=videofile, data=inputData, cookies={"session": session}, verify=False).json()
     except:
         page = None
     videoPath = page['path']
@@ -28,7 +28,7 @@ def test_classicSQLInjection():
 
     inputData = {'title': '(SELECT passhash FROM users WHERE username=\"jimstark\")', 'path': videoPath, 'description': 'This is a test video upload'}
     try:
-        page = requests.post("https://localhost/core/video/finalize.php", data=inputData, verify=False)
+        page = requests.post(url="https://localhost/core/video/finalize.php", data=inputData, verify=False)
     except:
         page = None
     assert page != None
