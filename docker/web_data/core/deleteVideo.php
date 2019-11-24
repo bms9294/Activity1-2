4a5eae0d223f18a9eeda55bee1778f530e6038ef
Brett Segraves <bms9294@rit.edu>
2019-11-24 14:13:28 -0500

    Added file deletion

--- docker/web_data/core/deleteVideo.php
+++ docker/web_data/core/deleteVideo.php
@@ -11,12 +11,21 @@ if($valid = '{"success": true}'){
         //echo $video."<br />";
         //echo "DELETE FROM videos WHERE username=
         //(SELECT userID FROM session WHERE token='".$token."') AND title=$video";
-        
+        $mysql = new MySqlClient("tables/video.php");
+        $mysql->connect();
+        $mysql->prepare("getVideo");
+        $vid = $mysql->exec([$video])->fetch();
+        //echo $vid['pathToVideo'];
         $link = mysqli_connect($config["host"],$config["username"],$config["password"],$config["db_name"]);
         //echo "<br />";
         if(mysqli_multi_query($link, "DELETE FROM videos WHERE userID=(SELECT userID FROM session WHERE token='".$token."') AND videoID=".$video)){
             echo '{"success": true}';
         }else{
+            echo '{"success": false}';
+        }
+        try{
+            unlink("../".$vid['pathToVideo']);
+        }catch(Exception $e){
             echo '{"success": false}';
         }
     }else{
